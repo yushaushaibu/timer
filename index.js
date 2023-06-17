0143107edc94e5c25548a870904387fa7cd69a64
unknown <yushaushaibu1@gmail.com>
2023-06-17 16:24:22 +0000

    How to use class around DOM elements

---
+++ index.js
@@ -0,0 +1,16 @@
+// How to call a constructor of a class.
+// class Timer {
+//     constructor(message) {
+//         console.log(message);
+//     };
+// };
+
+// HOW TO USE OR CLASS AROUND DOM ELEMENTS | Vid6
+/** 
+ * - Call a constructor of a class**
+ * - Add arguments to the constructor
+ * - Store a reference (variable) to each argument in the constructor method
+ * - Add event listeners to the reference variables to call methods in the class
+ * - Create elements in HTML elements, select the elements (in index.js)
+ * - Create instance of the timer and pass in the three arguments
+ */
