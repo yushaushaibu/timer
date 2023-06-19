5e8a9f282c1d060debd5fe92eec185a9d867b01f
unknown <yushaushaibu1@gmail.com>
2023-06-19 15:14:42 +0000

    How to connect variables and methods of a class to HTML DOM elements

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # timer
 build a timer app using the basic JavaScript concepts of functions and classes
+
+`constructor` is a very special function. It get called automatically whenever an instance of a class is created.
--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-// How to call a constructor of a class.
-// class Timer {
-//     constructor(message) {
-//         console.log(message);
-//     };
-// };
+/** WORKING WITH A CLASS AROUND DOM ELEMENTS | Vid6
+(How to connect (instance variables and methods) of a class to HTML DOM Elements)
 
-// HOW TO USE OR CLASS AROUND DOM ELEMENTS | Vid6
-/** 
- * - Call a constructor of a class**
+ * - Create "Timer" class and call a constructor (instance) of the class**
+ * 
  * - Add arguments to the constructor
- * - Store a reference (variable) to each argument in the constructor method
- * - Add event listeners to the reference variables to call methods in the class
- * - Create elements in HTML elements, select the elements (in index.js)
- * - Create instance of the timer and pass in the three arguments
+ * - Assign instance variables to the arguments.
+ * - Create a "start()" method in the class.
+ * - Add event listener to the instance variable (this.startButton) that invokes the start() method.
+ * - Create elements in HTML elements, select the elements (in index.js) 
+ * - Create an instance of the "Timer" class and pass in the selected elements as argument.
  */
+
+
+class Timer {
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener("click", this.start);
+    }
+
+    start() {
+        console.log("The timer is starting..")
+    };
+};
+
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+
+const timer = new Timer(durationInput, startButton, pauseButton);
