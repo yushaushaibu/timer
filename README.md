72cc54f746f754c18006f1ccc1c7d15b23d040df
unknown <yushaushaibu1@gmail.com>
2023-07-09 18:51:27 +0000

    the value of this in different context

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 build a timer app using the basic JavaScript concepts of functions and classes
 
 `constructor` is a very special function. It get called automatically whenever an instance of a class is created.
+
+the value of `this` points to the window object when it's referenced in global console.log, regular function and arrow function. However, the value of `this` in a method of an object points to the object itself.
+
+`this` inside of a method of a class allows access to the instance variables or/and methods of that particular class.
--- index.js
+++ index.js
@@ -9,25 +9,52 @@
  * - Add event listener to the instance variable (this.startButton) that invokes the start() method.
  * - Create elements in HTML elements, select the elements (in index.js) 
  * - Create an instance of the "Timer" class and pass in the selected elements as argument.
+ * 
+ * 
+ * THE VALUE OF "this" DEPENDING ON THE CONTEXT | Vid7 & 8
+ * The context of "this" in global console.log, function and arrow function ~ all points to the window object.
+ * 
+ * the value of "this" in a method of an object points to the object.
  */
 
 
-class Timer {
-    constructor(durationInput, startButton, pauseButton) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
+// class Timer {
+//     constructor(durationInput, startButton, pauseButton) {
+//         this.durationInput = durationInput;
+//         this.startButton = startButton;
+//         this.pauseButton = pauseButton;
 
-        this.startButton.addEventListener("click", this.start);
-    }
+//         this.startButton.addEventListener("click", this.start);
+//     }
 
-    start() {
-        console.log("The timer is starting..")
-    };
-};
+//     start() {
+//         console.log(this)
+//     };
+// };
 
-const durationInput = document.querySelector("#duration");
-const startButton = document.querySelector("#start");
-const pauseButton = document.querySelector("#pause");
+// const durationInput = document.querySelector("#duration");
+// const startButton = document.querySelector("#start");
+// const pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+// const timer = new Timer(durationInput, startButton, pauseButton);
+// timer.start();
+
+
+// console.log("GLOBAL", this);
+
+// function showThis() {
+//     console.log("FUNCTION", this)
+// };
+// showThis();
+
+// const printThis = () => {
+//     console.log("ARROW FUNCTION", this)
+// };
+// printThis();
+
+// const color = {
+//     printColor() {
+//         console.log(this)
+//     }
+// };
+// color.printColor();
