1b80e539bfd587e47ad28dee29fdeb22bfbc8f2b
unknown <yushaushaibu1@gmail.com>
2023-07-09 19:54:40 +0000

    this is used to access variables and methods in a class

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ build a timer app using the basic JavaScript concepts of functions and classes
 the value of `this` points to the window object when it's referenced in global console.log, regular function and arrow function. However, the value of `this` in a method of an object points to the object itself.
 
 `this` inside of a method of a class allows access to the instance variables or/and methods of that particular class.
+
+In order to call variables or methods in other methods in a class, assgin the variable with `this`. Example, this.intervalId.
--- index.js
+++ index.js
@@ -15,46 +15,46 @@
  * The context of "this" in global console.log, function and arrow function ~ all points to the window object.
  * 
  * the value of "this" in a method of an object points to the object.
+ * 
+ * 
+ * HOW TO SHARE VARIABLES AND METHODS AMONG METHODS IN THE SAME CLASS | Vid10
+ * create tick(), call it in start() and add setInterval.
+ * create pause(), call clearInterval() and assign pauseButton to event listener.
+ *  Attach "this" to the method or variable that you want to call in another method in the same class.
+ * 
+ * 
  */
 
 
-// class Timer {
-//     constructor(durationInput, startButton, pauseButton) {
-//         this.durationInput = durationInput;
-//         this.startButton = startButton;
-//         this.pauseButton = pauseButton;
+class Timer {
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
 
-//         this.startButton.addEventListener("click", this.start);
-//     }
+        this.startButton.addEventListener("click", this.start);
 
-//     start() {
-//         console.log(this)
-//     };
-// };
+        this.pauseButton.addEventListener("click", this.pause)
+    };
 
-// const durationInput = document.querySelector("#duration");
-// const startButton = document.querySelector("#start");
-// const pauseButton = document.querySelector("#pause");
+    start = () => {
+        this.tick();
+        this.intervalId = setInterval(this.tick, 1000);
+    };
 
-// const timer = new Timer(durationInput, startButton, pauseButton);
-// timer.start();
+    tick = () => {
+        console.log("tick..")
+    }
 
+    pause = () => {
+        clearInterval(this.intervalId);
+        console.log("Timer is paused!")
+    }
 
-// console.log("GLOBAL", this);
+};
 
-// function showThis() {
-//     console.log("FUNCTION", this)
-// };
-// showThis();
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
 
-// const printThis = () => {
-//     console.log("ARROW FUNCTION", this)
-// };
-// printThis();
-
-// const color = {
-//     printColor() {
-//         console.log(this)
-//     }
-// };
-// color.printColor();
+const timer = new Timer(durationInput, startButton, pauseButton);
